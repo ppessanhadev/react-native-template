44589215b0750dc3ddf3c4da8184ec5eed2afea9
Pablo Pessanha <pablo.pessanha@warren.com.br>
2022-01-05 22:07:02 -0300

    feat: adds lint files and configuration

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['warn'],
+    'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never' }],
+    'react/react-in-jsx-scope': ['off'],
+    'linebreak-style': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extesions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
